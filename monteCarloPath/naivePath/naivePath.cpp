7f4986fd5c156899881e0695955ebe48fe8eaf13
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-10-16 16:19:18 +0200

    Codice per naive sampling dei cammini, potenziale armonico

--- monteCarloPath/naivePath/naivePath.cpp
+++ monteCarloPath/naivePath/naivePath.cpp
@@ -12,6 +12,7 @@ using namespace std;
 //              1° riga     --> beta
 //              2° riga     --> numero di completezze
 //              3° riga     --> numero di iterazioni
+//              4° riga     --> delta (per intervallo mosse)
 
 
 // Leggo la configurazione iniziale
@@ -60,12 +61,13 @@ void parametriSimulativi(string nome, vector<double> &contenitore){
     filein.clear();
     filein.seekg(0,ios::beg);
 
-    if(conta != 4){
+    if(conta != 5){
         cout << "File dei parametri con input errati!" << endl;
         cout << "Formato richiesto: " << endl;
         cout << "Beta" << endl; 
         cout << "Numero completezze" << endl; 
         cout << "Numero iterazioni" << endl; 
+        cout << "Delta" << endl; 
         exit(-1);
     }
 
@@ -106,9 +108,68 @@ void stampaPar(const vector<double> &contenitore){
         cout << "Beta: " << contenitore[0] << endl;
         cout << "Numero di completezze: " << int(contenitore[1]) << endl;
         cout << "Numero di iterazioni: " << int(contenitore[2]) << endl;
+        cout << "Delta (intervallo mosse): " << contenitore[3] << endl;
         cout << endl;
     }
 }
+
+// Funzione gaussiana
+double gaussiana(double x, double x1, double sigma) {
+    double coeff = 1.0 / (sigma * sqrt(2.0 * M_PI)); 
+    double exponent = - (pow((x - x1), 2)) / (2 * pow(sigma, 2));  
+    return coeff * exp(exponent);
+}
+
+// Algoritmo per fare sampling del cammino di singola particella che vogliamo descrivere
+int mossaCammino(double dt, double delta, TRandom* generatore, vector<double> & config){
+    
+    // Scelgo un certo indice k (che corrisponde ad una certa evoluzione immaginaria) e 
+    // vado a valutare quali siano i primi vicini
+    int k = generatore -> Integer(size(config));
+    int knext = k + 1; int kprev = k - 1;
+    if(kprev == -1){kprev = size(config) - 1;}
+
+    // Propongo una nuova posizione estratta uniformemente  nell'intervallo centrato in x_k
+    // e delimitato dal parametro delta dornito come input
+    double propPos = config[k] + generatore -> Uniform(-delta, delta);
+
+    // Valuto se la mossa può essere accettata o meno lavorando con un algoritmo di metropolis
+    // Calcolo i pesi delle due configurazioni
+    double oldWeight = gaussiana(config[kprev], config[k], sqrt(dt)) * gaussiana(config[k], config[knext], sqrt(dt)) * exp(-dt*pow(config[k], 2)/2.0); //Peso mossa vecchia
+    double newWeight = gaussiana(config[kprev], propPos, sqrt(dt)) * gaussiana(propPos, config[knext], sqrt(dt)) * exp(-dt*pow(propPos, 2)/2.0); //Peso mossa nuova 
+    
+    // Valuto se accettare o meno
+    if(generatore -> Uniform(0, 1) < newWeight/oldWeight){
+        config[k] = propPos;
+        return 1;
+    }
+
+    return 0;
+}
+
+// Metodo per la formazione dell'istogramma
+void istoPosizioni(vector<int> &histo, double pos) {
+
+    double appo = pos + 4;
+    int index = int(appo * 20);
+    
+    if(index < 159){ histo[index] += 1; }
+}
+
+// Metodo per stampare l'istogramma
+void stampaHisto(string nome, const vector<int> &histo, int Niter) {
+
+    ofstream fileout;
+    fileout.open(nome);
+
+    for(int i = 0; i<int(size(histo)); i++) {
+        fileout << double(histo[i])/Niter << endl;
+    }
+
+    fileout.close();
+}
+
+
 
 int main(int argc, char* argv[]){
 
@@ -120,17 +181,41 @@ int main(int argc, char* argv[]){
     string nome_confIn = argv[1];
     string nome_par = argv[2];
 
-    vector<double> contieni;
+    vector<double> config;
     vector<double> paramIn;
     TRandom3* generator = new TRandom3();
 
     // Leggo la configurazione iniziale
-    inizializzaCammino(nome_confIn, contieni);
-    stampaConfig(contieni);
+    inizializzaCammino(nome_confIn, config);
+    stampaConfig(config);
 
     // Leggo i parametri iniziali
     parametriSimulativi(nome_par, paramIn);
     stampaPar(paramIn);
+
+    // Parametri per la simulazione
+    double beta = paramIn[0];
+    int Ncompl = int(paramIn[1]);
+    int Niter = int(paramIn[2]);
+    double delta = paramIn[3];
+
+    // Contenitore per istogramma e studio accettazione delle mosse
+    int accRate = 0;
+    vector<int> histo(160, 0);
+
+    double dt = beta/Ncompl;    //Intervallo di tempo immaginario
+    for(int i=0; i<Niter; i++){
+        accRate += mossaCammino(dt, delta, generator, config); 
+        istoPosizioni(histo, config[0]);
+    }
+
+    cout << endl;
+    cout << "Acceptance rate: " << endl;
+    cout << double(accRate)/Niter << endl;
+
+    cout << endl;
+    cout << "Stampa dell'istogramma a file: histo.dat" << endl;
+    stampaHisto("histo.dat", histo, Niter);
 
     delete generator;
     return 0;
