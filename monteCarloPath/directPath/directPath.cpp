0727d90605f2429f9955f2a01545e2a74e50f3c6
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-10-16 18:46:44 +0200

    Algoritmo di sampling diretto: la condizione iniziale fornita potrebbe essere errata

--- monteCarloPath/directPath/directPath.cpp
+++ monteCarloPath/directPath/directPath.cpp
@@ -12,8 +12,7 @@ using namespace std;
 //              1° riga     --> beta
 //              2° riga     --> numero di completezze
 //              3° riga     --> numero di iterazioni
-//              4° riga     --> delta (per intervallo mosse)
-//              5° riga     --> starting point 
+//              4° riga     --> starting point 
 
 
 // Leggo parametri simulativi
@@ -37,13 +36,12 @@ void parametriSimulativi(string nome, vector<double> &contenitore){
         conta++;
     }
 
-    if(conta != 6){
+    if(conta != 5){
         cout << "File dei parametri con input errati!" << endl;
         cout << "Formato richiesto: " << endl;
         cout << "Beta" << endl; 
         cout << "Numero completezze" << endl; 
         cout << "Numero iterazioni" << endl; 
-        cout << "Delta" << endl; 
         cout << "Starting point catena" << endl; 
         exit(-1);
     }
@@ -73,7 +71,6 @@ void stampaPar(const vector<double> &contenitore){
         cout << "Beta: " << contenitore[0] << endl;
         cout << "Numero di completezze: " << int(contenitore[1]) << endl;
         cout << "Numero di iterazioni: " << int(contenitore[2]) << endl;
-        cout << "Delta (intervallo mosse): " << contenitore[3] << endl;
         cout << "Starting point: " << contenitore[4] << endl;
         cout << endl;
     }
@@ -84,6 +81,30 @@ double gaussiana(double x, double x1, double sigma) {
     double coeff = 1.0 / (sigma * sqrt(2.0 * M_PI)); 
     double exponent = - (pow((x - x1), 2)) / (2 * pow(sigma, 2));  
     return coeff * exp(exponent);
+}
+
+// Algoritmo per fare sampling del cammino di singola particella che vogliamo descrivere
+// Levy harmonic algorithm, in questo caso consideriamo starting ed ending point come coincidenti
+void mossaCammino(double dt, int Nid, TRandom* generatore, vector<double> &config){
+    
+    //Scelgo punto d'inizio per il cammino
+    double start = config[1];
+    double end = config[1];
+    config[0] = start;
+
+    //Calcolo elementi necessari per gaussiana
+    double y1, y2;
+    double mean, sigma;
+
+    for(int i=1; i<int(size(config)); i++){
+        y1 = 1/tanh(dt) + 1/tanh((Nid - i) * dt);
+        y2 = start/sinh(dt) + end/sinh((Nid - i) * dt);
+
+        mean = y2/y1; sigma = 1/sqrt(y1);
+        config[i] = mean + generatore -> Gaus(0, sigma);
+
+        start = config[i];
+    }
 }
 
 // Metodo per la formazione dell'istogramma
@@ -114,18 +135,15 @@ void stampaHisto(string nome, const vector<int> &histo, int Niter) {
 
 int main(int argc, char* argv[]){
 
-    if(argc != 3) { 
-        cout << "Utilizzo programma: " << argv[0] << " <file configurazione iniziale> <file parametri>" << endl;
+    if(argc != 2) { 
+        cout << "Utilizzo programma: " << argv[0] << "<file parametri>" << endl;
         return -1;
     }
 
-    string nome_confIn = argv[1];
-    string nome_par = argv[2];
+    string nome_par = argv[1];
 
-    vector<double> config;
     vector<double> paramIn;
     TRandom3* generator = new TRandom3();
-
 
     // Leggo i parametri iniziali
     parametriSimulativi(nome_par, paramIn);
@@ -135,21 +153,16 @@ int main(int argc, char* argv[]){
     double beta = paramIn[0];
     int Ncompl = int(paramIn[1]);
     int Niter = int(paramIn[2]);
-    double delta = paramIn[3];
+    vector<double> config(Ncompl, 0); config[0] = paramIn[3];
 
     // Contenitore per istogramma e studio accettazione delle mosse
-    int accRate = 0;
     vector<int> histo(80, 0);
 
     double dt = beta/Ncompl;    //Intervallo di tempo immaginario
     for(int i=0; i<Niter; i++){
-        accRate += 1; 
+        mossaCammino(dt, Ncompl, generator, config);
         istoPosizioni(histo, config[0]);
     }
-
-    cout << endl;
-    cout << "Acceptance rate: " << endl;
-    cout << double(accRate)/Niter << endl;
 
     cout << endl;
     cout << "Stampa dell'istogramma a file: histo.dat" << endl;
