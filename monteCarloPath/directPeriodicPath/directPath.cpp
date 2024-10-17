f683ea392291e96cd17049b6ff5837aca1ad64d3
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-10-17 12:17:00 +0200

    Inizio lo studio dei cammini periodici

---
+++ monteCarloPath/directPeriodicPath/directPath.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+#include <TRandom3.h>
+
+using namespace std;
+
+//  ATTENZIONE: il file param.dat contiene i parametri da fornire per la simulazione 
+//              1° riga     --> beta
+//              2° riga     --> numero di completezze
+//              3° riga     --> numero di iterazioni
+//              4° riga     --> starting point 
+
+
+// Leggo parametri simulativi
+void parametriSimulativi(string nome, vector<double> &contenitore){
+    
+    int conta = 0;
+    double appo = 0; string pippo;
+
+    ifstream filein;
+    filein.open(nome);
+
+    //Controllo corretta apertura del file
+    if(!filein) {
+        cerr << "Errore in apertura file che fornisce la configurazione di input." << endl;
+        cerr << "Termino esecuzione del programma" << endl;
+        exit(1);
+    }
+
+    while(!filein.eof()){
+        getline(filein, pippo);
+        conta++;
+    }
+
+    if(conta != 5){
+        cout << "File dei parametri con input errati!" << endl;
+        cout << "Formato richiesto: " << endl;
+        cout << "Beta" << endl; 
+        cout << "Numero completezze" << endl; 
+        cout << "Numero iterazioni" << endl; 
+        cout << "Starting point catena" << endl; 
+        exit(-1);
+    }
+
+    else{
+        filein.clear();
+        filein.seekg(0,ios::beg);
+
+        while(filein >> appo){
+            contenitore.push_back(appo); 
+        }
+    }
+
+    filein.close();
+}
+
+
+// Stampo i parametri della simulazione
+void stampaPar(const vector<double> &contenitore){
+    if(size(contenitore) == 0){
+        cout << "Contenitore dei parametri vuoto, non abbiamo alcun parametro simulativo!" << endl;
+    }
+
+    else{
+        cout << endl;
+        cout << "Parametri per la simulazione" << endl;
+        cout << "Beta: " << contenitore[0] << endl;
+        cout << "Numero di completezze: " << int(contenitore[1]) << endl;
+        cout << "Numero di iterazioni: " << int(contenitore[2]) << endl;
+        cout << "Starting point: " << contenitore[4] << endl;
+        cout << endl;
+    }
+}
+
+// Funzione gaussiana
+double gaussiana(double x, double x1, double sigma) {
+    double coeff = 1.0 / (sigma * sqrt(2.0 * M_PI)); 
+    double exponent = - (pow((x - x1), 2)) / (2 * pow(sigma, 2));  
+    return coeff * exp(exponent);
+}
+
+
+// Metodo per la formazione dell'istogramma
+void istoPosizioni(vector<int> &histo, double pos) {
+
+    double appo = pos + 4;
+    int index = int(appo * 10);
+    
+    if(index <= 79){ histo[index] += 1; }
+}
+
+// Metodo per stampare l'istogramma
+void stampaHisto(string nome, const vector<int> &histo, int Niter) {
+
+    ofstream fileout;
+    fileout.open(nome);
+
+    double appo = 0;
+    for(int i = 0; i<int(size(histo)); i++) {
+        appo = double(histo[i])/Niter;
+        fileout << appo << endl;
+    }
+
+    fileout.close();
+}
+
+
+
+int main(int argc, char* argv[]){
+
+    if(argc != 2) { 
+        cout << "Utilizzo programma: " << argv[0] << "<file parametri>" << endl;
+        return -1;
+    }
+
+    string nome_par = argv[1];
+
+    vector<double> paramIn;
+    TRandom3* generator = new TRandom3();
+
+    // Leggo i parametri iniziali
+    parametriSimulativi(nome_par, paramIn);
+    stampaPar(paramIn);
+
+    // Parametri per la simulazione
+    double beta = paramIn[0];
+    int Ncompl = int(paramIn[1]);
+    int Niter = int(paramIn[2]);
+    vector<double> config(Ncompl, 0); config[0] = paramIn[3];
+
+    // Contenitore per istogramma e studio accettazione delle mosse
+    vector<int> histo(80, 0);
+
+    double dt = beta/Ncompl;    //Intervallo di tempo immaginario
+    for(int i=0; i<Niter; i++){
+        istoPosizioni(histo, config[0]);
+    }
+
+    cout << endl;
+    cout << "Stampa dell'istogramma a file: histo.dat" << endl;
+    stampaHisto("histo.dat", histo, Niter);
+
+    delete generator;
+    return 0;
+}
